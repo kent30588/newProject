2732b6b4f4d5fe34e1a39dfdc116e0f973e6b0af
unknown <ak4raulov@yandex.ru>
2021-08-17 22:51:12 +0300

    решил задачу циклом while

--- src/js/script.js
+++ src/js/script.js
@@ -25,37 +25,39 @@
 
 'use strict';
 
+/*
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const lastМiewedЬovie = prompt('Один из последних просмотренных фильмов?', '');
+const value = prompt('На сколько оцените его?', '');
+const lastМiewedЬovie2 = prompt('Один из последних просмотренных фильмов?', '');
+const value2 = prompt('На сколько оцените его?', '');
 
-//const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-//const lastМiewedЬovie = prompt('Один из последних просмотренных фильмов?', '');
-//const value = prompt('На сколько оцените его?', '');
-//const lastМiewedЬovie2 = prompt('Один из последних просмотренных фильмов?', '');
-//const value2 = prompt('На сколько оцените его?', '');
 
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {
+		lastМiewedЬovie, value, lastМiewedЬovie2, value2 
+	},
+	actors: {},
+	genres: [],
+	privat: false
+};
 
-//const personalMovieDB = {
-//	count: numberOfFilms,
-//	movies: {
-//		lastМiewedЬovie, value, lastМiewedЬovie2, value2 
-//	},
-//	actors: {},
-//	genres: [],
-//	privat: false
-//};
+console.log(personalMovieDB);
+*/
 
-//console.log(personalMovieDB);
 
 //Правильное решение
 
-//const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-//const personalMovieDB = {
-//	count: numberOfFilms,
-//	movies: {},
-//	actors: {},
-//	genres: [],
-//	privat: false
-//};
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
 
 //let a = prompt('Один из последних просмотренных фильмов?', '');
@@ -66,7 +68,7 @@
 //personalMovieDB.movies[a] = b;
 //personalMovieDB.movies[c] = d;
 
-//console.log(personalMovieDB);
+console.log(personalMovieDB);
 
 
 /* Задание на урок:
@@ -85,52 +87,66 @@
 
 // Частичное мое решения 1 -2 задач
 
-
-//for (let i = 1; i < 3; i++){
-//	const a = prompt('Один из последних просмотренных фильмов?', '');
-//		if (a === '') {
-//			alert('введите большее количество символов');	
-//		}
-//	const b = prompt('На сколько оцените его?', '');
-//	personalMovieDB.movies[a] = b;
-//}
-//console.log(personalMovieDB);
-
+/*
+for (let i = 1; i < 3; i++){
+	const a = prompt('Один из последних просмотренных фильмов?', '');
+		if (a === '') {
+			alert('введите большее количество символов');	
+		}
+	const b = prompt('На сколько оцените его?', '');
+	personalMovieDB.movies[a] = b;
+}
+console.log(personalMovieDB);
+*/
 
 
 //Правильное решение
-
+/*
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
 const personalMovieDB = {
-	count: numberOfFilms,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-for (let i = 0; i < 2; i++){
-	const a = prompt('Один из последних просмотренных фильмов?', '');
-	const b = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', '');
+    const b = prompt('На сколько оцените его?', '');
 
-	if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-		personalMovieDB.movies[a] = b;
-		console.log('Done');
-	} else {
-		console.log('Error');
-		i--;
-	}
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('Done');
+    } else {
+        console.log('Error');
+        i--;
+    }
 }
 
-if (personalMovieDB.count < 10 ) {
-	alert('"Просмотрено довольно мало фильмов"');
+if (personalMovieDB.count < 10) {
+    alert('"Просмотрено довольно мало фильмов"');
 } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-	alert("Вы классический зритель");
+    alert("Вы классический зритель");
 } else if (personalMovieDB.count >= 30) {
-	alert("Вы киноман");
+    alert("Вы киноман");
 } else {
-	alert("Произошла ошибка");
+    alert("Произошла ошибка");
 }
 
 console.log(personalMovieDB);
+*/
+
+// мое решение циклом while
+let i = 2;
+while (i) {
+    let a = prompt('Один из последних просмотренных фильмов?', '');
+    let b = prompt('На сколько оцените его?', '');
+    i--;
+    if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i++;
+    }
+}
