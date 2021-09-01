b9108f440595437776fbc9ba6e2e423c7f03be81
unknown <ak4raulov@yandex.ru>
2021-09-01 14:30:53 +0300

    СОЗДАНИЕ КОПИЙ

--- src/js/script.js
+++ src/js/script.js
@@ -338,3 +338,86 @@ P.S. Функции вызывать не обязательно*/
 //const arr = [10, 13, 9, 1, 8, 12];
 //arr.sort(compareNum);
 //console.log(arr);
+//let a = 5;
+//let b = a;
+//b = b + 5;
+//console.log(b);
+//console.log(a);
+
+//const obj = {
+//    a: 5,
+//    b: 1
+//};
+//const copy = obj; // Ссылка на объект
+//copy.a = 10;
+//console.log(copy);
+//console.log(obj);
+
+//function copy(mainObj) {
+//    //создаем новый объект
+//    let objCopy = {};
+
+//    let key;
+//    for (key in mainObj) {
+//        objCopy[key] = mainObj[key];
+//    }
+//    return objCopy;
+//}
+
+//const numbers = {
+//    a: 2,
+//    b: 5,
+//    c: {
+//        x: 7,
+//        y: 4
+//    }
+//};
+
+//const newNumbers = copy(numbers); // клонирование объекта
+
+//newNumbers.a = 10;
+//console.log(newNumbers);
+//console.log(numbers);
+
+//const add = {
+//    t: 7,
+//    k: 2
+//};
+
+//const clone = Object.assign({}, add);
+//clone.t = 20;
+//console.log(clone);
+//console.log(add);
+
+//const oldArray = ['a', 'b', 'c'];
+//let copyArray = oldArray.slice();
+//copyArray[1] = 'asdasdasd';
+//console.log(oldArray);
+//console.log(copyArray);
+
+//const video = ['youtube', 'vimeo', 'rutube'];
+//const blogs = ['wordpress', 'livejournal', 'blogger'];
+//const internet = [...video, ...blogs, 'vk', 'facebook'];
+
+//console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 5, 7];
+log(...num);
+
+const array = ['a', 'b'];
+const newAarray = [...array];
+
+const q = {
+    one: 1,
+    two: 2,
+};
+
+const n = {...q };
+
+console.log(n);
