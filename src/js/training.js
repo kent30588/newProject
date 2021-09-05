6497a08b621e72a5502dcde09202b458b9a2d9ba
Alexandr <ak4raulov@yandex.ru>
2021-09-05 17:48:44 +0300

    действия с элементами на странице

--- src/js/training.js
+++ src/js/training.js
@@ -424,3 +424,30 @@
 
 //const oneHeart = document.querySelector('.heart');// получим первый элемент 
 //console.log(oneHeart);
+
+//                                                            Действия с элементами на странице
+
+//const box = document.getElementById('box');
+//const btns = document.getElementsByTagName('button');
+//const circles = document.getElementsByClassName('circle');
+//const hearts = document.querySelectorAll('.heart');
+//const oneHeart = document.querySelector('.heart');
+
+////console.dir(box);
+//box.style.backgroundColor = 'blue';//Инлайн стили
+//box.style.width = '500px';
+//box.style.cssText = 'background-color: blue; width: 500px;';
+//let width = 200;
+//box.style.cssText = `background-color: blue; width: ${width}px;`;
+//btns[1].style.borderRadius = '100%';
+//circles[0].style.backgroundColor = 'red';
+
+//for (let i = 0; i < hearts.length; i++){
+//	hearts[i].style.backgroundColor = 'blue';
+//}
+//hearts.forEach(item => {
+//	item.style.backgroundColor = 'blue';
+//});
+
+// Создаем элемент
+const div = document.createElement('div');
