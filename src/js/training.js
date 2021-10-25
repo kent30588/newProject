a18bbabfaebea50506edd613c1b873609066e6c6
Alexandr <ak4raulov@yandex.ru>
2021-10-25 18:56:49 +0300

    Параметры документа окна и работа с ними

--- src/js/training.js
+++ src/js/training.js
@@ -1232,10 +1232,31 @@
 //                       параметры документа, окна и работа с ними
 
 const box = document.querySelector('.box');
+const btn = document.querySelector('button');
+//const width = box.clientWidth;
+//const height = box.clientHeight;
 
-const width = box.clientWidth;
-const height = box.clientHeight;
+//const width = box.offsetWidth;
+//const height = box.offsetHeight;
+
+const width = box.scrollWidth;
+const height = box.scrollHeight;
 console.log(width, height);
+
+btn.addEventListener('click', () => {
+	box.style.height = box.scrollHeight + 'px';
+	//box.style.height = '100%';
+	console.log(box.scrollTop);
+});
+
+console.log(box.getBoundingClientRect().top);
+
+const style = window.getComputedStyle(box);
+
+console.log(style.display);
+
+console.log(document.documentElement.scrollTop);
+
 
 
 
