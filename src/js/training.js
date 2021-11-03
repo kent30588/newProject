433068f9b2524b06fdd0de7b328ea49cf3ab1475
Alexandr <ak4raulov@yandex.ru>
2021-11-03 18:44:48 +0300

    контекст вызова this

--- src/js/training.js
+++ src/js/training.js
@@ -1258,25 +1258,107 @@
 //console.log(document.documentElement.scrollTop);
 
 //                    Функции-Конструкторы	
-function User(name, id) {
-	this.name = name;
-	this.id = id;
-	this.human = true;
-	this.hello = function () {
-		console.log(`Hello ${this.name}`);
-	};
-}
-User.prototype.exit = function () {
-	console.log(`Пользователь ${this.name} ушел`);
-};
-const ivan = new User('Ivan', 28);
-const alex = new User('Alex', 20);
+//function User(name, id) {
+//	this.name = name;
+//	this.id = id;
+//	this.human = true;
+//	this.hello = function () {
+//		console.log(`Hello ${this.name}`);
+//	};
+//}
+//User.prototype.exit = function () {
+//	console.log(`Пользователь ${this.name} ушел`);
+//};
+//const ivan = new User('Ivan', 28);
+//const alex = new User('Alex', 20);
 
-ivan.exit();
+//ivan.exit();
 
-ivan.hello();
-alex.hello();
+//ivan.hello();
+//alex.hello();
 
-console.log(ivan);
-console.log(alex);
+//console.log(ivan);
+//console.log(alex);
 
+//                Контекст вызова This
+// 1) Обычная функция : this = window; но если стоит use strict = undefined
+//function showThis() {
+//	console.log(this);
+//}
+
+//showThis();
+
+//function showThis(a, b) {
+//	console.log(this);
+//	function sum() {
+//		console.log(this);
+//		return a + b;
+//	}
+//	console.log(sum());
+//}
+
+//showThis(4, 5);
+
+//2) Контекст у методов объекта - сам объект
+//const obj = {
+//	a: 2,
+//	b: 15,
+//	sum: function () {
+//		function shout() {
+//			console.log(this);
+//		}
+//		shout();
+//	}
+//};
+
+//obj.sum();
+
+//3)this в конструкторах и классах - это новый экземпляр объекта
+//function User(name, id) {
+//	this.name = name;
+//	this.id = id;
+//	this.human = true;
+//}
+//let ivan = new User('Ivan', 30);
+
+//4)Ручная привязка this: call, apply, bind
+//function sayName(surname) {
+//	console.log(this);
+//	console.log(this.name + surname);
+//}
+
+//const user = {
+//	name: 'Alex'
+//};
+
+//sayName.call(user, 'Smitt');
+//sayName.apply(user, ['Smitt']);
+
+//function count(num) {
+//	return this * num;
+//}
+
+//const double = count.bind(2);
+//console.log(double(3));
+//console.log(double(13));
+
+//const btn = document.querySelector('button');
+
+//btn.addEventListener('click', function() {
+//	this.style.backgroundColor = 'red';
+//});
+
+//const obj = {
+//	num: 5,
+//	sayNamber: function () {
+//		const say = () => {
+//			console.log(this.num);
+//		};
+//		say();
+//	}
+//};
+
+//obj.sayNamber();
+
+//const double = a => a * 2;
+//console.log(double(4));
