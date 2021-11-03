ddb35aae955e02b27a0003bb03f9c36a72995824
Alexandr <ak4raulov@yandex.ru>
2021-11-03 16:31:58 +0300

    функции конструкторы

--- src/js/training.js
+++ src/js/training.js
@@ -1231,34 +1231,52 @@
 
 //                       параметры документа, окна и работа с ними
 
-const box = document.querySelector('.box');
-const btn = document.querySelector('button');
+//const box = document.querySelector('.box');
+//const btn = document.querySelector('button');
 //const width = box.clientWidth;
 //const height = box.clientHeight;
 
 //const width = box.offsetWidth;
 //const height = box.offsetHeight;
 
-const width = box.scrollWidth;
-const height = box.scrollHeight;
-console.log(width, height);
+//const width = box.scrollWidth;
+//const height = box.scrollHeight;
+//console.log(width, height);
 
-btn.addEventListener('click', () => {
-	box.style.height = box.scrollHeight + 'px';
-	//box.style.height = '100%';
-	console.log(box.scrollTop);
-});
+//btn.addEventListener('click', () => {
+//	box.style.height = box.scrollHeight + 'px';
+//	//box.style.height = '100%';
+//	console.log(box.scrollTop);
+//});
 
-console.log(box.getBoundingClientRect().top);
+//console.log(box.getBoundingClientRect().top);
 
-const style = window.getComputedStyle(box);
+//const style = window.getComputedStyle(box);
 
-console.log(style.display);
+//console.log(style.display);
 
-console.log(document.documentElement.scrollTop);
+//console.log(document.documentElement.scrollTop);
 
+//                    Функции-Конструкторы	
+function User(name, id) {
+	this.name = name;
+	this.id = id;
+	this.human = true;
+	this.hello = function () {
+		console.log(`Hello ${this.name}`);
+	};
+}
+User.prototype.exit = function () {
+	console.log(`Пользователь ${this.name} ушел`);
+};
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
 
+ivan.exit();
 
+ivan.hello();
+alex.hello();
 
-
+console.log(ivan);
+console.log(alex);
 
