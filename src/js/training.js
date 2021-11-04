759d44a6ab751293d40b16d6dd07b1897d2f5b3d
Alexandr <ak4raulov@yandex.ru>
2021-11-04 12:20:39 +0300

    классы

--- src/js/training.js
+++ src/js/training.js
@@ -1362,3 +1362,139 @@
 
 //const double = a => a * 2;
 //console.log(double(4));
+
+
+//Классы (ES6)
+
+//class Rectangle {
+//	constructor(height, width) {
+//		this.height = height;
+//		this.width = width;
+//	}
+//	calcArea() {
+//		return this.height * this.width;
+//	}
+//}
+
+//class ColoredRectangleWithText extends Rectangle {
+//	constructor(height, width, text, bgColor) {
+//		super(height,width);
+//		this.text = text;
+//		this.bgColor = bgColor;
+//	}
+//	showMyProps() {
+//		console.log(`Текс: ${this.text}, цвет ${this.bgColor}`);
+//	}
+//}
+
+//const div = new ColoredRectangleWithText(25, 10, 'Hello World', 'red');
+//div.showMyProps();
+//console.log(div.calcArea());
+
+//const squere = new Rectangle(15, 15);
+//const a = new Rectangle(50, 23);
+
+//console.log(squere.calcArea());
+//console.log(a.calcArea());
+
+//const animal = {
+//	name: 'Animal',
+//	age: 5,
+//	hasTail: true
+//};
+
+//class Animal {
+
+//	static type = 'ANIMAL';
+
+//	constructor(options) {
+//		this.name = options.name;
+//		this.age = options.age;
+//		this.hasTail = options.hasTail;
+//	}
+//	voice() {
+//		console.log('I am Animal');
+//	}
+//}
+
+////const animal = new Animal({
+////	name: 'Animal',
+////	age: 5,
+////	hasTail: true
+////});
+
+
+//class Cat extends Animal {
+//	static type = 'CAT'
+//	constructor(options) {
+//		super(options);
+//		this.color = options.color;
+//	}
+//	voise() {
+//		super.voice();
+//		console.log('I am Cat');
+//	}
+//	get ageInfo() {
+//		return this.age * 7;
+//	}
+//	set ageInfo(newAge) {
+//		this.age = newAge;
+//	}
+//};
+//const cat = new Cat({
+//	name: 'Cat',
+//	age: 7,
+//	hasTail: true,
+//	color: 'Black'
+//});
+
+class Component {
+	constructor(selector) {
+		this.$el = document.querySelector(selector);
+	}
+	hide() {
+		this.$el.style.display = 'none';
+	}
+	show() {
+		this.$el.style.display = 'block';
+	}
+}
+
+class Box extends Component {
+	constructor(options) {
+		super(options.selector);
+			this.$el.style.width = this.$el.style.height = options.size + 'px';
+			this.$el.style.backgroundColor= options.color;
+		
+	}
+}
+
+const box1 = new Box({
+	selector: '#box1',
+	size: 100,
+	color: 'red'
+});
+
+const box2 = new Box({
+	selector: '#box2',
+	size: 120,
+	color: 'blue'
+});
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', function () {
+	box1.hide();
+});
+
+class Circle extends Box{
+	constructor(options) {
+		super(options);
+		this.$el.style.borderRadius = '50%';
+	}
+}
+
+const c = new Circle({
+	selector: '#circle',
+	size: 90,
+	color: 'green'
+});
