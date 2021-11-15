c1289f395424d0495b851adccbfbf2a7a65777a0
Alexandr <ak4raulov@yandex.ru>
2021-11-15 18:36:42 +0300

    AJAX и общение с сервером

--- src/js/training.js
+++ src/js/training.js
@@ -1518,6 +1518,28 @@
 //console.log(clone);
 
 
+//                                          AJAX и общение с сервером
+
+const inputRub = document.querySelector('#rub'),
+	inputUsd = document.querySelector('#usd');
+
+inputRub.addEventListener('input', () => {
+	const request = new XMLHttpRequest();
+
+	request.open('GET', 'js/current.json');
+	request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+	request.send();
+	request.addEventListener('load', () => {
+		if (request.status === 200) {
+			//console.log(request.response);
+			const data = JSON.parse(request.response);
+			//console.log(data);
+			inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+		} else {
+			inputUsd.value = 'Попробуйте чуть позже';
+		}
+	});
+});
 
 
 
