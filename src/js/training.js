aa8d894dbc3ab64222dc989118b0370c93424f68
Alexandr <ak4raulov@yandex.ru>
2021-12-07 10:04:04 +0300

    абота с localStorage

--- src/js/training.js
+++ src/js/training.js
@@ -1641,6 +1641,46 @@
 //console.log(newArr);
 
 
+                                    // Работа с localStorage
+										
+const checkbox = document.querySelector('#checkbox');
+const form = document.querySelector('form');
+const change = document.querySelector('#color');
+
+if (localStorage.getItem('isChecked')) {
+	checkbox.checked = true;
+}
+
+if (localStorage.getItem('bg') === 'changed') {
+	form.style.backgroundColor = 'red';
+}
+
+checkbox.addEventListener('change', () => {
+	localStorage.setItem('isChecked', true);
+});
+
+change.addEventListener('click', () => {
+	if (localStorage.getItem('bg') === 'changed') {
+		localStorage.removeItem('bg');
+		form.style.backgroundColor = '#fff';
+	} else {
+		localStorage.setItem('bg', 'changed');
+		form.style.backgroundColor = 'red';
+	}
+});
+
+const persone = {
+	name: 'Alex',
+	age: 25
+};
+
+const serializedPersone = JSON.stringify(persone);
+
+localStorage.setItem('Alex', persone);
+
+console.log(localStorage.getItem('Alex'));
+
+
 
 
 
