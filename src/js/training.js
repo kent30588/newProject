fae9b02a4104954f598fdbb29185453c93bc93dc
Alexandr <ak4raulov@yandex.ru>
2021-12-07 16:27:47 +0300

    геттеры и сетторы

--- src/js/training.js
+++ src/js/training.js
@@ -1704,6 +1704,23 @@
 //console.log(str.match(/\s/ig));
 
 
+//                  геттеры и сеттеры (свойства объектов)
+
+//const persone = {
+//	name: 'Alex',
+//	age: 25,
+
+//	get userAge() {
+//		return this.age;
+//	},
+
+//	set userAge(num) {
+//		this.age = num;
+//	}
+//};
+
+//console.log(persone.userAge = 30);
+//console.log(persone.userAge);
 
 
 
