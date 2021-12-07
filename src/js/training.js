3c90ab88010a4d35ba06f1e5e49778206587a5c3
Alexandr <ak4raulov@yandex.ru>
2021-12-07 18:15:14 +0300

    инкапсуляция

--- src/js/training.js
+++ src/js/training.js
@@ -1722,6 +1722,56 @@
 //console.log(persone.userAge = 30);
 //console.log(persone.userAge);
 
+//                             Инкопсуляция
+//код без инкапсуляции
+/*
+function User(name, age) {
+	this.name = name;
+	this.age = age;
+
+	this.say = function () {
+		console.log(`Имя пользователя ${this.name}, возраст ${this.age}`);
+	};
+}
+
+const ivan = new User('Ivan', 27);
+console.log(ivan.name);
+console.log(ivan.age);
+
+ivan.age = 30;
+ivan.name = 'Alex';
+ivan.say();
+*/
+//Тот же код с инкапсуляцией
+
+class User {
+	constructor (name, age){
+		this.name = name;
+		this._age = age;
+	}
+
+	say() {
+		console.log(`Имя пользователя ${this.name}, возраст ${this._age}`);
+	}
+
+	get age() {
+		return this._age;
+	}
+
+	set age(age) {
+		if (typeof age === 'number' && age > 0 && age < 110) {
+			this._age = age;
+		} else {
+			console.log('Недопустимое значение');
+		}
+	}
+}
+
+const ivan = new User('Ivan', 27);
+console.log(ivan.age);
+ivan.age = 99;
+console.log(ivan.age);
+ivan.say();
 
 
 
